27a1e5e93d5c1629942e8808a84ae4b44ca22401
Nattapong <nattapong.paungpool@gmail.com>
2024-03-13 23:27:17 +0700

    resize / update db

--- public/js/app.js
+++ public/js/app.js
@@ -41,8 +41,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/dist/web/pusher.js");
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'pusher',
-  key: "747db8e1a1de6230bc8a",
-  cluster: "ap1",
+  key: "",
+  cluster: "mt1",
   forceTLS: true
 });
 
